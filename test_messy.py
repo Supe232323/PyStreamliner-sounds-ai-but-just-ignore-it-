d93c0ad93c9299f3e34ada6c77db6b2e92977e67
Supe232323 <bigjosh3703@gmail.com>
2026-03-03 13:35:39 -0600

    Add test_messy.py so we can test the cleaner

---
+++ test_messy.py
@@ -0,0 +1,24 @@
+import math
+import random
+import time
+
+name = "bro"
+age = 12
+
+x = 10
+if x > 5:
+    print("big number")
+else:
+    print("small number")
+
+y = x * 2
+y = x * 2   # this line is repeated on purpose
+
+score = 100
+print("Your score is", score)
+
+z = 50
+if z == 50:
+    print("exactly 50")
+else:
+    print("not 50")
